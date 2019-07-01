212ce30b4b12c4cfcfa7712e855e53551f89d492
USUARIO <USUARIO@DESKTOP-9ODPV3J>
2019-07-01 10:17:28 -0500

    version 1 prueba de integracion

---
+++ PruebaIntegracion/src/pruebaintegracion/PruebaIntegracion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaintegracion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PruebaIntegracion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primera version");
+        // TODO code application logic here
+    }
+    
+}
